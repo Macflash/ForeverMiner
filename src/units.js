6622892978e298f939fb7457b68813b85b8ae881
Macflash <alex.brofo@outlook.com>
2015-08-13 10:28:35 -0500

    added basic files

---
+++ src/units.js
@@ -0,0 +1,70 @@
+// JavaScript source code
+
+var Radius = {PROJECTILE: 3, TURRET: 15, MINER: 30, ENEMY: 10};
+
+var Projectile = function (x, y, dir, stats) {
+    this.x = x;
+    this.y = y;
+    this.dir = dir;
+    this.speed = 1;
+    this.damage = 1;
+    this.splash = 1;
+    this.augment = null;
+    this.radius = Radius.PROJECTILE;
+}
+
+var Turret = function (x, y) {
+    // EXP AND LEVEL
+    this.exp = 0;
+    this.level = 0;
+
+    // CORE STAT LEVELS
+    this.speed = 1; //how fast you shoot
+    this.precision = 1; //how far and accurate you shoot
+    this.damage = 1; //how much you do to the first hit target
+    this.splash = 1; //how big the splash is and how much it does
+
+    // AUGMENT SLOT
+    this.augment = null;
+
+    // WORLD INFO
+    this.x = x;
+    this.y = y;
+    this.dir = 0;
+    this.radius = Radius.TURRET;
+}
+
+var Enemy = function (x, y) {
+    // LEVEL
+    this.level = 0;
+
+    // CORE STAT LEVELS
+    this.health = 1;
+    this.dodge = 1;
+    this.armor = 1;
+    this.moveSpeed = 1;
+    this.damage = 1;
+    this.regen = 1;
+    this.attackSpeed = 1;
+    this.radius = Radius.ENEMY;
+}
+
+var Miner = function (x, y) {
+    // EXP AND LEVEL
+    this.level = 0;
+
+    // CORE STAT LEVELS
+    this.speed = 1; // how fast it mines minerals
+    this.storage = 1; // how much it can store at one time
+    this.transfer = 1; // how fast it can transfer minerals to orbit
+    this.health = 1;
+    this.armor = 1;
+
+    // AUGMENT SLOT
+    this.augment = null;
+
+    // WORLD INFO
+    this.x = x;
+    this.y = y;
+    this.radius = Radius.MINER;
+}
